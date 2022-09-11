a34bbbafdc2411044a7377132aa7366afffe68ec
xiangliheart <xiangliheart@163.com>
2022-09-12 00:03:22 +0800

    add modules eob-platform-workflow、eob-platform-commonservice

---
+++ eob/eob-platform/eob-platform-commonservice/src/main/java/com/xiangliheart/eob/platform/commonservice/EobPlatformCommonServiceApplication.java
@@ -0,0 +1,23 @@
+/**
+ * Copyright (c) 2022-2022 xiangliheart(湘澧寸心) All rights reserved.
+ */
+
+package com.xiangliheart.eob.platform.commmonservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * EobPlatformCommoServiceApplication
+ *
+ * @auther: xiangliheart(湘澧寸心)
+ * @since: 2022/9/12
+ */
+@SpringBootApplication
+public class EobPlatformCommoServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EobPlatformWorkflowApplication.class, args);
+    }
+
+}
---
+++ eob/eob-platform/eob-platform-workflow/src/main/java/com/xiangliheart/eob/platform/workflow/EobPlatformWorkflowApplication.java
@@ -0,0 +1,23 @@
+/**
+ * Copyright (c) 2022-2022 xiangliheart(湘澧寸心) All rights reserved.
+ */
+
+package com.xiangliheart.eob.platform.workflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * EobPlatformWorkflowApplication
+ *
+ * @auther: xiangliheart(湘澧寸心)
+ * @since: 2022/9/12
+ */
+@SpringBootApplication
+public class EobPlatformWorkflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EobPlatformWorkflowApplication.class, args);
+    }
+
+}
